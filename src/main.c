5b79137486ed4369a34b1631d96264fc780fc706
FreaxMATE <konstantin.unruh@mnet-mail.de>
2019-09-01 17:39:51 +0200

    fix too many arguments libvlc_init

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ int main(int argc, char **argv)
    gtk_init (&argc, &argv) ;
 
    setupWindow() ;
-   initVlc(playerWidget) ;
+   initVlc() ;
    gtk_widget_show(window);
    gtk_main() ;
    quitVlc() ;
