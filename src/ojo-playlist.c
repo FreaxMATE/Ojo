881744c93c3abddf75e43f91393be76cece394ac
WilburWetterquarz <49864414+FreaxMATE@users.noreply.github.com>
2019-12-12 20:16:08 +0100

    seperate OjoTrack and argv handling (#16)
    free gslist members
    
    standardize opening process (command line/ filemanager + gtkfilechooser)
    
    display right title in lieu of <Unknown>
    
    add scrollbar to playlist

--- src/ojo-playlist.c
+++ src/ojo-playlist.c
@@ -27,11 +27,12 @@ OjoPlaylist *ojo_playlist_initialize()
 {
    OjoPlaylist *new ;
    new = malloc (sizeof(OjoPlaylist)) ;
-   new->playlist_box = GTK_LIST_BOX(gtk_builder_get_object(builder, "ojo_playlist_box")) ;
+   new->playlist_box = GTK_BOX(gtk_builder_get_object(builder, "ojo_playlist_box")) ;
+   new->playlist_listbox = GTK_LIST_BOX(gtk_builder_get_object(builder, "ojo_playlist_listbox")) ;
    return new ;
 }
 
-void on_ojo_playlist_box_row_activated(GtkListBox *box, GtkListBoxRow *row, gpointer user_data)
+void on_ojo_playlist_listbox_row_activated(GtkListBox *box, GtkListBoxRow *row, gpointer user_data)
 {
    ojo_player_media_play(gtk_list_box_row_get_index(row)) ;
 }
@@ -55,7 +56,7 @@ void ojo_playlist_gtk_initialize()
       playlist_widgets[i] = gtk_label_new(ojo_player_get_title(i)) ;
       gtk_label_set_xalign(GTK_LABEL(playlist_widgets[i]), 0.0) ;
       gtk_widget_show(playlist_widgets[i]) ;
-      gtk_list_box_insert(ojo_playlist->playlist_box, playlist_widgets[i], i) ;
+      gtk_list_box_insert(ojo_playlist->playlist_listbox, playlist_widgets[i], i) ;
    }
 }
 
@@ -63,7 +64,7 @@ void ojo_playlist_entries_remove()
 {
    GList *children, *iter;
 
-   children = gtk_container_get_children(GTK_CONTAINER(ojo_playlist->playlist_box)) ;
+   children = gtk_container_get_children(GTK_CONTAINER(ojo_playlist->playlist_listbox)) ;
    for (iter = children; iter != NULL; iter = g_list_next(iter))
       gtk_widget_destroy(GTK_WIDGET(iter->data)) ;
    g_list_free(children) ;
@@ -72,7 +73,7 @@ void ojo_playlist_entries_remove()
 void ojo_playlist_show()
 {
    if (!gtk_widget_is_visible(GTK_WIDGET(ojo_playlist->playlist_box)))
-      gtk_widget_show(GTK_WIDGET(ojo_playlist->playlist_box)) ;
+      gtk_widget_show_all(GTK_WIDGET(ojo_playlist->playlist_box)) ;
 }
 
 void ojo_playlist_hide()
@@ -83,7 +84,7 @@ void ojo_playlist_hide()
 
 void ojo_playlist_select_row(int index)
 {
-   gtk_list_box_select_row (ojo_playlist->playlist_box, gtk_list_box_get_row_at_index(ojo_playlist->playlist_box, index)) ;
+   gtk_list_box_select_row (ojo_playlist->playlist_listbox, gtk_list_box_get_row_at_index(ojo_playlist->playlist_listbox, index)) ;
 }
 
 
